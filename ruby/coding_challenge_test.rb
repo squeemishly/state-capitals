d084cede68fda105161b62fc8d07b0e5e3c73a1f
squeemishly <squeemishly@gmail.com>
2017-09-28 19:58:09 -0600

    sets up attr_reader for template for faster test writing

--- ruby/coding_challenge_test.rb
+++ ruby/coding_challenge_test.rb
@@ -4,10 +4,13 @@ require 'minitest/autorun'
 require_relative './coding_challenge'
 
 class ObjectTest < Minitest::Test
-  def test_it_exists
-    
-    template = TemplateClass.new
-    assert_instance_of TemplateClass, template
+  attr_reader :template
 
+  def setup
+    @template = TemplateClass.new
+  end
+
+  def test_it_exists
+    assert_instance_of TemplateClass, template
   end
 end
