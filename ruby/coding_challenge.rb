713c162d79c6d877e6b41b06425d381813312a93
squeemishly <squeemishly@gmail.com>
2017-10-02 20:36:34 -0600

    returns the abbreviation for a state

--- ruby/coding_challenge.rb
+++ ruby/coding_challenge.rb
@@ -1,4 +1,21 @@
 require 'pry'
 
 class StateCapitals
+  attr_reader :states,
+              :capitals
+
+  def initialize
+    @states = {"Oregon" => "OR",
+              "Alabama" => "AL",
+              "New Jersey" => "NJ",
+              "Colorado" => "CO"}
+    @capitals = {"OR" => "Salem",
+                "AL" => "Montgomery",
+                "NJ" => "Trenton",
+                "CO" => "Denver"}
+  end
+
+  def find_abbreviation(state)
+    states[state]
+  end
 end
