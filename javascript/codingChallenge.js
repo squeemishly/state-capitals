cfb6b2cd997d7d788e22c703eea92e5c2375ea9d
squeemishly <squeemishly@gmail.com>
2017-10-02 21:09:40 -0600

    returns the state abbreviation from the state name

--- javascript/codingChallenge.js
+++ javascript/codingChallenge.js
@@ -1,9 +1,19 @@
 class StateCapitals {
   constructor() {
+    this.states = {"Oregon": "OR",
+              "Alabama": "AL",
+              "New Jersey": "NJ",
+              "Colorado": "CO"};
 
+    this.capitals = {"OR": "Salem",
+                "AL": "Montgomery",
+                "NJ": "Trenton",
+                "CO": "Denver"};
   }
 
-
+  state_to_abbreviation(state) {
+    return this.states[state]
+  }
 }
 
 module.exports = StateCapitals;
