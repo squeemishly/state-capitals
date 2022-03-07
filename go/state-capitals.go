1ead705dfa9f3bf6fb15e7835922c7fcccd07017
squee <squeemishly@gmail.com>
2022-03-07 11:01:41 -0700

    move states declarations to structs create function to find capitals

--- go/state-capitals.go
+++ go/state-capitals.go
@@ -5,6 +5,25 @@ import (
 	"os"
 )
 
+type State struct {
+	name         string
+	abbreviation string
+	capital      string
+}
+
+var states = []State{
+	State{
+		name:         "Oregon",
+		abbreviation: "OR",
+		capital:      "Portland",
+	},
+	State{
+		name:         "Colorado",
+		abbreviation: "CO",
+		capital:      "Denver",
+	},
+}
+
 func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("gimme an state!")
@@ -14,14 +33,44 @@ func main() {
 	state := os.Args[1]
 
 	abbrev := stateToAbbrev(state)
-	fmt.Printf("The abbreviation for Oregon is %s", abbrev)
+	if abbrev == "" {
+		fmt.Println("We don't know that state. Try again.")
+		os.Exit(1)
+	} else {
+		fmt.Printf("The abbreviation for %s is %s", state, abbrev)
+	}
+
+	fmt.Println()
+
+	capital := stateCapital(state)
+	if capital == "" {
+		fmt.Println("We don't know that state. Try again.")
+		os.Exit(1)
+	} else {
+		fmt.Printf("The capital for %s is %s", state, capital)
+	}
 }
 
 func stateToAbbrev(state string) string {
-	stateAbbrev := map[string]string{
-		"Oregon":   "OR",
-		"Colorado": "CO",
+	var abbrev string
+	for _, s := range states {
+		if s.name == state {
+			abbrev = s.abbreviation
+			break
+		}
 	}
 
-	return stateAbbrev[state]
+	return abbrev
+}
+
+func stateCapital(state string) string {
+	var capital string
+	for _, s := range states {
+		if s.name == state {
+			capital = s.capital
+			break
+		}
+	}
+
+	return capital
 }
